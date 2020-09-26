f43b353282f441c34123500f50a3444ee62b779d
LeDaniii <danielmatzer@gmail.com>
2020-09-26 14:12:12 +0200

    automatic card generation linked with json

--- Js/team.js
+++ Js/team.js
@@ -1,14 +1,17 @@
 // const memberName = document.getElementById("member-name");
 // const email = document.getElementById("email");
 
+var teamJson = JSON.parse(teamMembers);
+console.table(teamJson);
+
 function displayCard() {
-  for (let i = 0; i <= 4; i++) {
+  for (let i = 0; i < teamJson.length; i++) {
     let teamSection = document.getElementById("team");
     teamSection.innerHTML += `<div class="member-card" id="member-card${i}"></div>`;
     let memberCard = document.getElementById(`member-card${i}`);
-    memberCard.innerHTML += `<img src="../img/placeholder1.png">`;
-    memberCard.innerHTML += `<h4 id="member-name">Daniel Matzer</h4>`;
-    memberCard.innerHTML += `<p id="email">daniel.matzer@piagroup.com</p>`;
+    memberCard.innerHTML += `<img src=${teamJson[i].image}>`;
+    memberCard.innerHTML += `<h4 id="member-name">${teamJson[i].name}</h4>`;
+    memberCard.innerHTML += `<p id="email">${teamJson[i].email}</p>`;
   }
 }
 
